358d69f614d37affa228b060d78306be276829f0
Odaia Lacombe <odaialcmb@gmail.com>
2017-05-09 11:37:41 +0200

    adding CRUD to coktail_controller

--- app/controllers/cocktail_controller.rb
+++ app/controllers/cocktail_controller.rb
@@ -1,2 +1,35 @@
 class CocktailController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(params_cocktail)
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def update
+    @cocktail = Cocktail.find(params[:id])
+    redirect_to coktail_path(@cocktail)
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def params_cocktail
+    params.require(:cocktail).permit(:name)
+  end
 end

b3ccadfd4d063264aeef2c321fe55b7432a402f1
Odaia Lacombe <odaialcmb@gmail.com>
2017-05-10 10:45:40 +0200

    adding new seeds with json

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
-require 'faker'
+require 'open-uri'
+require 'json'
 
 puts 'Cleaning database...'
 Ingredient.destroy_all
 
-ingredients = %w(lemon ice mint leaves redbull jagermeister sugar tonic gin rhum)
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(url).read
+list = JSON.parse(ingredients_serialized)
+
+ingredients = list["drinks"].map { |hash| hash["strIngredient1"] }
+
 ingredients.each { |ingredient| Ingredient.create(name: ingredient) }
 
 
